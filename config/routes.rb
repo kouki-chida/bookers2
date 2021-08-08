4f07464df7570799eb9e20150cbe8a5539d31dfa
kouki-chida <k.chida.1024@icloud.com>
2021-08-08 14:02:31 +0000

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,11 @@ Rails.application.routes.draw do
    resources :book_comments, only: [:create, :destroy]
   end
 
-  resources :users
-
+  resources :users do
+   resource :relationships, only: [:create, :destroy]
+  get "followings" => "relationships#followings", as: "followings"
+  get "followers" => "relationships#followers", as: "followers"
+end
 
 end
 
