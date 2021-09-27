92bfdcd2ed6f1b52279411260079309f05045bfb
kouki-chida <k.chida.1024@icloud.com>
2021-09-27 13:09:25 +0000

    検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'search/search'
   devise_for :users
   get "home/about" => "homes#about"
 
   root to: 'homes#top'
-   resources :books, only: [:new, :create, :index, :show, :destroy] do
+   resources :books, only: [:new, :create, :index, :show, :edit, :destroy] do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
   end
@@ -14,6 +15,8 @@ Rails.application.routes.draw do
   get "followings" => "relationships#followings", as: "followings"
   get "followers" => "relationships#followers", as: "followers"
 end
+
+  get '/search' => 'search#search'
 
 end
 
